2e4ed2bb5104f5ab0a7f430a06316faa1dd5ad81
Frank Calise <fcalise@gmail.com>
2025-05-15 16:15:17 -0400

    reanimated patch for mocking useScrollViewOffset

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,28 +1,10 @@
 import "react-native-gesture-handler/jestSetup";
-// import '@testing-library/jest-native/extend-expect';
-
-jest.mock("react-native-reanimated", () => {
-  const Reanimated = require("react-native-reanimated/mock");
-
-  // The mock for `call` immediately calls the callback which is incorrect
-  // So we override it with a no-op
-  Reanimated.default.call = () => {};
-
-  /**
-   * This didn't work, had to go to `node_modules/react-native-reanimated/mock.js` and
-   * add useScrollViewOffset: () => ({ value: 0 }),
-   */
-  // Reanimated.useScrollViewOffset = () => ({
-  //   value: 0,
-  // });
-
-  return Reanimated;
-});
+require("react-native-reanimated").setUpTests();
 
 jest.mock("expo-font");
 
 // Mock TurboModuleRegistry
-// jest.mock('react-native/Libraries/TurboModule/TurboModuleRegistry');
+// jest.mock("react-native/Libraries/TurboModule/TurboModuleRegistry");
 // Mock NativeEventEmitter
 jest.mock("react-native/Libraries/EventEmitter/NativeEventEmitter");
 // Mock Settings
